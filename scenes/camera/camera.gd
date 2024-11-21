d010e92b2f827490748b11fe5a2a30d2acd3c4d6
Kodie <kodii.tulip@gmail.com>
2024-11-20 21:22:19 -0300

    change raycast detection to use Plane
    no longer relying on a physics body to detect when right clicked on the world.

--- scenes/camera/camera.gd
+++ scenes/camera/camera.gd
@@ -1,54 +1,64 @@
 class_name CameraController
 extends Node3D
 
-@export var movement_time: float = 5.0
+@export_group("Speed", "speed_")
 @export var speed_normal: float = 0.5
 @export var speed_fast: float = 3.0
+@export_group("Zoom", "zoom_")
+@export var zoom_amount: float = 10.0
+@export var zoom_max: float = 200.0
+@export var zoom_min: float = 50.0
+@export var movement_time: float = 5.0
 @export var rotation_amount: float = 2.0
-@export var zoom_amount: Vector3 = Vector3(0, -10, 10)
 
-var movement_speed: float
 var new_position: Vector3
 var new_rotation: Quaternion
-var new_zoom: Vector3:
-	set(value):
-		new_zoom = Vector3(
-			value.x,
-			clampf(value.y, 80, 2500),
-			clampf(value.z, -2500, -80)
-			)
+var new_zoom: float
 var drag_start_pos: Vector3
 var drag_current_pos: Vector3
 
-@onready var camera: Camera3D = $Camera3D
+@onready var camera: Camera3D = $SpringArm3D/Camera3D
+@onready var camera_arm: SpringArm3D = $SpringArm3D
 
 
 func _ready() -> void:
 	new_position = global_position
 	new_rotation = Quaternion(transform.basis)
-	new_zoom = camera.position
+	new_zoom = camera_arm.spring_length
 
 
 func _process(delta: float) -> void:
 	_handle_movement_input()
-	_handle_movement_mouse_input()
 	_handle_rotation_input()
 	_handle_zoom_input()
+	_handle_movement_mouse_input()
+
+	new_zoom = clampf(new_zoom, zoom_min, zoom_max)
+	#new_position.y = 10.0
 
 	global_position = global_position.lerp(new_position, delta * movement_time)
 	transform.basis = transform.basis.slerp(Basis(new_rotation.normalized()),
 		delta * movement_time)
-	camera.position = camera.position.lerp(new_zoom, delta * movement_time)
+	camera_arm.spring_length = lerpf(camera_arm.spring_length,
+		new_zoom, delta * movement_time)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"mouse_zoom_in"):
-		new_zoom += zoom_amount
-	if event.is_action_pressed(&"mouse_zoom_out"):
 		new_zoom -= zoom_amount
+	if event.is_action_pressed(&"mouse_zoom_out"):
+		new_zoom += zoom_amount
 
 	if event is InputEventMouseMotion:
 		_handle_rotation_mouse_input(event as InputEventMouseMotion)
+
+
+func _cast_ray_from_cam(cam: Camera3D, ray_length: float) -> Variant:
+	var plane: Plane = Plane(Vector3.UP).normalized()
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	var origin: Vector3 = cam.project_ray_origin(mouse_pos)
+	var end: Vector3 = origin + cam.project_ray_normal(mouse_pos) * ray_length
+	return plane.intersects_segment(origin, end)
 
 
 func _handle_rotation_mouse_input(event: InputEventMouseMotion) -> void:
@@ -60,33 +70,18 @@ func _handle_rotation_mouse_input(event: InputEventMouseMotion) -> void:
 
 func _handle_movement_mouse_input() -> void:
 	if Input.is_action_just_pressed(&"move_cam_mouse"):
-		var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-		var mouse_pos: Vector2 = get_viewport().get_mouse_position()
-		var origin: Vector3 = camera.project_ray_origin(mouse_pos)
-		var end: Vector3 = origin + camera.project_ray_normal(mouse_pos) * 2500
-		var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(
-			origin, end)
-		var result: Dictionary = space_state.intersect_ray(query)
-		drag_start_pos = result["position"] \
-			if not result.is_empty() \
-			else Vector3.ZERO
-
+		var pos: Variant = _cast_ray_from_cam(camera, new_zoom * 2)
+		if pos:
+			drag_start_pos = pos
 	if Input.is_action_pressed(&"move_cam_mouse"):
-		var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-		var mouse_pos: Vector2 = get_viewport().get_mouse_position()
-		var origin: Vector3 = camera.project_ray_origin(mouse_pos)
-		var end: Vector3 = origin + camera.project_ray_normal(mouse_pos) * 2500
-		var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(
-			origin, end)
-		var result: Dictionary = space_state.intersect_ray(query)
-		drag_current_pos = result["position"] \
-			if not result.is_empty() \
-			else Vector3.ZERO
+		var pos: Variant = _cast_ray_from_cam(camera, new_zoom * 2)
+		if pos:
+			drag_current_pos = pos
 		new_position = global_position + drag_start_pos - drag_current_pos
 
 
 func _handle_zoom_input() -> void:
-	var axis: float = Input.get_axis(&"zoom_out",&"zoom_in")
+	var axis: float = Input.get_axis(&"zoom_in",&"zoom_out")
 	new_zoom += axis * zoom_amount
 
 
@@ -96,14 +91,16 @@ func _handle_rotation_input() -> void:
 
 
 func _handle_movement_input() -> void:
-	movement_speed = speed_fast \
-		if Input.is_action_pressed(&"fast_cam_moviment") \
-		else speed_normal
+	var movement_speed: float = speed_fast \
+			if Input.is_action_pressed(&"fast_cam_moviment") \
+			else speed_normal
+
 	var dir: Vector2 = Input.get_vector(
-		&"move_cam_left",
-		&"move_cam_right",
-		&"move_cam_forward",
-		&"move_cam_backward"
-		)
+			&"move_cam_left", &"move_cam_right",
+			&"move_cam_forward", &"move_cam_backward")
+
 	var new_transform: Vector3 = (transform.basis * -Vector3(dir.x, 0.0, dir.y))
-	new_position += new_transform.normalized() * movement_speed
+	var speed_modfier: float = remap(new_zoom, zoom_min, zoom_max,
+		1, zoom_max / zoom_min)
+
+	new_position += new_transform.normalized() * movement_speed * speed_modfier
